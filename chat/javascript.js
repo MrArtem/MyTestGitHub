e83cdcaba9f522f036051e04ab065ade5c7151a0
MrArtem <artem6695@mail.ru>
2015-03-11 17:42:49 +0300

    modified chat and added new jquery files

--- chat/javascript.js
+++ chat/javascript.js
@@ -1,16 +1,34 @@
+var messageOption = function(text,value) {
+    return {
+        message: text,
+        id: value
+    };
+};
+var infoLogin = function (name, surname) {
+    return {
+        name: name,
+        surname: surname
+    };
+};
+var listForSaving = [];
 function run() {
     var appContainerSend = document.getElementById('send');
     var appContainerDelete = document.getElementById('delete');
     var appContainerChange = document.getElementById('change');
     var appContainerSelect = document.getElementById('allMessages');
-    var appContainerSignIn = document.getElementById('signin');
-    
+    var appContainerServer = document.getElementById('server');
+
+    var allMessages = restoreMessages();
+    var loginInfo = restoreLoginInfo();
+    createAllMessages(allMessages);
+    createLoginInfo(loginInfo);
+
     appContainerSend.addEventListener('click', delegateEventSend);
     appContainerDelete.addEventListener('click', delegateEventDelete);
     appContainerChange.addEventListener('click', delegateEventChange);
     appContainerSelect.addEventListener('click', delegateEventSelect);
     appContainerSignIn.addEventListener('click', delegateEventSignIn);
-
+    appContainerServer.addEventListener('click', delegateEventServer);
 }
 function delegateEventSend(evtObj) {
     var text = document.getElementById('sendText');
@@ -22,8 +40,13 @@ function delegateEventSend(evtObj) {
         var option = document.createElement("option");
         option.text = surname.value + " " + name.value + " : " + text.value;
         option.value = select.length;
-     
+
         select.add(option);
+
+        listForSaving.push( messageOption(option.text, option.value));
+        storeMessages(listForSaving);
+        storeInfoLogin(infoLogin(name.value,surname.value));
+
         text.value = "";
     }
 }
@@ -51,20 +74,72 @@ function delegateEventChange(evtObj) {
     select.text = surname.value + " " + name.value + " : " + sendText.value;
     sendText.value = "";
 }
-function delegateEventSignIn() {
-    var chatArt = document.getElementById('ChatArt');
-    var Login = document.getElementById('Login');
 
+function delegateEventServer(evtObj) {
+    $("#server").removeClass('btn btn-success');
+    $("#server").addClass('btn btn-danger');
+}
+function storeMessages(listForSaving) {
+    if (typeof (Storage) == "undefined") {
+        alert('localStorage is not accessible');
+        return;
+    }
+    localStorage.setItem("list messages", JSON.stringify(listForSaving));
+}
+function storeInfoLogin(infoLogin) {
+    if (typeof (Storage) == "undefined") {
+        alert('localStorage is not accessible');
+        return;
+    }
+    localStorage.setItem("Login info", JSON.stringify(infoLogin));
+}
+function restoreMessages(){
+    if (typeof (Storage) == "undefined") {
+        alert('local storage is not accessible');
+        return;
+    }
+    var item = localStorage.getItem("list messages");
+    return item && JSON.parse(item);
+}
+function restoreLoginInfo() {
+    if (typeof (Storage) == "undefined") {
+        alert('local storage is not accessible');
+        return;
+    }
+    var item = localStorage.getItem("Login info");
+    return item && JSON.parse(item);
+}
+function createAllMessages(allMessages) {
+    for (var i = 0; i < allMessages.length; i++) {
+        listForSaving.push(allMessages[i]);
+        addAllMessages(allMessages[i]);
+    }
+}
+function createLoginInfo(loginInfo) {
     var inputName = document.getElementById('inputName');
     var inputSurName = document.getElementById('inputSurName');
 
-    var name = document.getElementById('name');
-    var surname = document.getElementById('surname');
-
-    name.value = inputName.value;
-    surname.value = inputSurName.value;
-
-    chatArt.className = "chat";
-    Login.className = Login.className + " hiddenDiv";
-       
+    inputName.value = loginInfo.name;
+    inputSurName.value = loginInfo.surname;
 }
+function addAllMessages(message) {
+    var select = document.getElementById('allMessages');
+    var option = document.createElement("option");
+    option.text = message.message;
+    option.value = message.id;
+
+    select.add(option);
+}
+
+$(document).ready(function () {
+
+    $(document).on('click', '#Rename', function () {
+        var d = $('#Login');
+        var dOpt = {
+            title: 'Login',
+            modal: true,
+            resizable: false,
+        };
+        d.dialog(dOpt);
+    });
+});
